f0b54f608a45de4aecf2e2be635a81a1fa52f53c
Arseniy Dorogin <dorogin.arseniy@yandex.ru>
2022-04-08 14:01:51 +0300

    Initial Commit

---
+++ MVVM-R_Financial_example_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-R_Financial_example_app
+//
+//  Created by Arseniy Dorogin on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
